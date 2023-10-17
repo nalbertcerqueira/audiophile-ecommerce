728bac2c42d40c138f3d934716066eb93f59ecc5
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-10-17 19:16:13 -0300

    feat: create product entity

---
+++ src/@core/backend/domain/entities/product.ts
@@ -0,0 +1,38 @@
+export interface Accessory {
+    item: string
+    quantity: number
+}
+
+export interface ImageMap {
+    mobile: string
+    tablet: string
+    desktop: string
+}
+
+export interface ProductGallery {
+    first: ImageMap
+    second: ImageMap
+    third: ImageMap
+}
+
+export interface RelatedProduct {
+    slug: string
+    name: string
+    category: string
+    image: ImageMap
+}
+
+export interface ProductProps {
+    id: string
+    slug: string
+    name: string
+    category: string
+    new: boolean
+    price: number
+    description: string
+    features: string
+    includes?: Accessory[]
+    others: RelatedProduct[]
+    image: ImageMap
+    gallery: ProductGallery
+}
