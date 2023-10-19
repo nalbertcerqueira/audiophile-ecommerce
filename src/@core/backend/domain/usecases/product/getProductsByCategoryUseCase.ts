41df36c5e2391b5bfb8493972d59754c686f3ad5
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-10-19 19:28:12 -0300

    refactor: add readonly accessor to the product usescases dependencies

--- src/@core/backend/domain/usecases/product/getProductsByCategoryUseCase.ts
+++ src/@core/backend/domain/usecases/product/getProductsByCategoryUseCase.ts
@@ -2,7 +2,9 @@ import { ProductProps } from "../../entities/product"
 import { GetProductsByCategoryRepository } from "../../repositories/getProductsByCategoryRepository"
 
 export class GetProductsByCategoryUseCase {
-    constructor(private getProductsByCategoryRepository: GetProductsByCategoryRepository) {}
+    constructor(
+        private readonly getProductsByCategoryRepository: GetProductsByCategoryRepository
+    ) {}
 
     public async execute(category: string): Promise<ProductProps[]> {
         const products = await this.getProductsByCategoryRepository.getByCategory(category)
--- src/@core/backend/domain/usecases/product/getProductsUseCase.ts
+++ src/@core/backend/domain/usecases/product/getProductsUseCase.ts
@@ -2,7 +2,7 @@ import { ProductProps } from "../../entities/product"
 import { GetProductsRepository } from "../../repositories/getProductsRepository"
 
 export class GetProductsUseCase {
-    constructor(private getProductsRepository: GetProductsRepository) {}
+    constructor(private readonly getProductsRepository: GetProductsRepository) {}
 
     public async execute(): Promise<ProductProps[]> {
         const products = await this.getProductsRepository.getAll()
