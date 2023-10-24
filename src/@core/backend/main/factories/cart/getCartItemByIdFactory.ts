abbbcdd205ba04c96a60d07d6fac43e196f3c984
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-10-24 12:16:36 -0300

    refactor: make the factory export a singleton instead of the factory method

--- src/@core/backend/main/factories/cart/getCartItemByIdFactory.ts
+++ src/@core/backend/main/factories/cart/getCartItemByIdFactory.ts
@@ -1,8 +1,10 @@
 import { GetCartItemsByIdUseCase } from "@/@core/backend/domain/usecases/cart/getCartItemByIdUseCase"
 import { MongoCartItemRepository } from "@/@core/backend/infra/db/mongo/repositories/cart/mongoCartItemRepository"
 
-export function createGetCartItemByIdUseCase() {
+function createGetCartItemByIdUseCase() {
     const mongoCartItemsRepository = new MongoCartItemRepository()
     const getCartItemByIdUseCase = new GetCartItemsByIdUseCase(mongoCartItemsRepository)
     return getCartItemByIdUseCase
 }
+
+export const getCartItemByIdUseCase = createGetCartItemByIdUseCase()
--- src/@core/backend/main/factories/cart/getCartItemsFactory.ts
+++ src/@core/backend/main/factories/cart/getCartItemsFactory.ts
@@ -1,8 +1,10 @@
 import { GetCartItemsUseCase } from "@/@core/backend/domain/usecases/cart/getCartItemsUseCase"
 import { MongoCartItemRepository } from "@/@core/backend/infra/db/mongo/repositories/cart/mongoCartItemRepository"
 
-export function createGetCartItemsUseCase() {
+function createGetCartItemsUseCase() {
     const mongoCartItemsRepository = new MongoCartItemRepository()
     const getCartItemsUseCase = new GetCartItemsUseCase(mongoCartItemsRepository)
     return getCartItemsUseCase
 }
+
+export const getCartItemsUseCase = createGetCartItemsUseCase()
