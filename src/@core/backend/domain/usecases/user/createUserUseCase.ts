b4218b963f6a80c70aa4feafbcfc80e4d7d48a52
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-11-03 11:45:06 -0300

    feat: create CreateUserUseCase class to help with user signup

---
+++ src/@core/backend/domain/usecases/user/createUserUseCase.ts
@@ -0,0 +1,33 @@
+import { UserProps, User } from "../../entities/user"
+import { AddUserRepository } from "../../repositories/user/addUserRepository"
+import { FindUserByEmailRepository } from "../../repositories/user/findUserByEmailRepository"
+import { HashService } from "../../services/hashService"
+
+type UserData = Pick<UserProps, "email" | "name" | "password">
+
+export class CreateUserUseCase {
+    constructor(
+        private readonly findUserByEmailRepository: FindUserByEmailRepository,
+        private readonly addUserRepository: AddUserRepository,
+        private readonly hashService: HashService
+    ) {}
+    public async execute(userData: UserData): Promise<User | null> {
+        const { email, name, password } = userData
+        const foundUser = await this.findUserByEmailRepository.findByEmail(email)
+
+        if (!foundUser) {
+            const hashedPassword = await this.hashService.hash(password)
+            const newUser = {
+                name,
+                email,
+                password: hashedPassword,
+                images: { profile: null, profileThumb: null }
+            }
+            const addedUser = await this.addUserRepository.add(newUser)
+
+            return addedUser
+        }
+
+        return null
+    }
+}
