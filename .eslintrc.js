af3e55d0d4900607cff681ad36e57451c3d0fe1d
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-11-04 17:39:09 -0300

    chore: add 'jose' dependency to help with jwt tokens and fix some identations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,48 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "next/core-web-vitals",
-    "prettier",
-    "plugin:prettier/recommended",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
     },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint", "react", "prettier"],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/explicit-member-accessibility": [
-      "error",
-      {
-        accessibility: "explicit",
-        overrides: {
-          accessors: "explicit",
-          constructors: "off",
-          methods: "explicit",
-          properties: "explicit",
-          parameterProperties: "explicit",
-        },
-      },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "next/core-web-vitals",
+        "prettier",
+        "plugin:prettier/recommended"
     ],
-  },
-};
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script"
+            }
+        }
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: ["@typescript-eslint", "react", "prettier"],
+    rules: {
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/explicit-member-accessibility": [
+            "error",
+            {
+                accessibility: "explicit",
+                overrides: {
+                    accessors: "explicit",
+                    constructors: "off",
+                    methods: "explicit",
+                    properties: "explicit",
+                    parameterProperties: "explicit"
+                }
+            }
+        ]
+    }
+}
