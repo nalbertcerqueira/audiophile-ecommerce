948471fd25730cfdc0bbf1693123ec0098a71131
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-11-04 17:36:07 -0300

    feat: create the LoginUseCase implementation

---
+++ src/@core/backend/domain/usecases/auth/loginUseCase.ts
@@ -0,0 +1,34 @@
+import { UserProps } from "../../entities/user/user"
+import { FindUserByEmailRepository } from "../../repositories/user/findUserByEmailRepository"
+import { HashComparerService } from "../../services/hashComparerService"
+import { TokenGeneratorService } from "../../services/tokenGeneratorService"
+
+type LoginData = Pick<UserProps, "email" | "password">
+
+export class LoginUseCase {
+    constructor(
+        private readonly findUserByEmailRepository: FindUserByEmailRepository,
+        private readonly hashComparer: HashComparerService,
+        private readonly tokenGenerator: TokenGeneratorService
+    ) {}
+    public async execute(loginData: LoginData): Promise<string | null> {
+        const { email, password } = loginData
+        const foundUser = (await this.findUserByEmailRepository.findByEmail(email))?.toJSON()
+
+        if (foundUser) {
+            const isPasswordCorrect = await this.hashComparer.compare(
+                password,
+                foundUser.password
+            )
+
+            if (isPasswordCorrect) {
+                const payload = { id: foundUser.id }
+                const accessToken = await this.tokenGenerator.generate(payload)
+
+                return accessToken
+            }
+        }
+
+        return null
+    }
+}
