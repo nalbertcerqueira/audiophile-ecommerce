71a01fba8a364bc34723b5af969f917e4ca7ffb2
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-11-04 12:19:15 -0300

    feat: create signup route

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,47 @@
+import { zodErrorFormater } from "@/@core/backend/infra/validators/zod/zod-helpers"
+import { addUserUseCase } from "@/@core/backend/main/factories/user/createUserFactory"
+import { NextResponse } from "next/server"
+import {
+    passwordMessage,
+    passwordRegexp,
+    userZodSchema
+} from "@/@core/backend/domain/entities/user/utils"
+import z from "zod"
+
+const signupValidator = userZodSchema
+    .pick({ name: true, email: true, password: true })
+    .extend({
+        passwordConfirmation: z
+            .string()
+            .min(8)
+            .trim()
+            .refine((password) => password.match(passwordRegexp), passwordMessage)
+    })
+    .refine((data) => data.password === data.passwordConfirmation, {
+        message: "passwords don't match"
+    })
+
+export async function POST(req: Request) {
+    try {
+        const body = await req.json()
+        const validationResult = signupValidator.safeParse(body)
+
+        if (validationResult.success) {
+            const { name, email, password } = validationResult.data
+            const newUser = await addUserUseCase.execute({ name, email, password })
+            if (newUser) {
+                return NextResponse.json({ data: newUser }, { status: 201 })
+            }
+
+            return NextResponse.json(
+                { errors: [`user with email ${body.email} is already registered`] },
+                { status: 409 }
+            )
+        }
+
+        const badRequestErrors = zodErrorFormater(validationResult.error)
+        return NextResponse.json({ errors: badRequestErrors }, { status: 400 })
+    } catch (error: any) {
+        return NextResponse.json({ errors: [error.message] }, { status: 500 })
+    }
+}
