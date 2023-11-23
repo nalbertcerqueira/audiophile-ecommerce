2a60580a3ba43242cf1d20df558fffa1cb7a79b2
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-11-23 20:36:05 -0300

    refactor: make Input component accept refs and remove unnecessary code

--- src/components/checkout/components/Input.tsx
+++ src/components/checkout/components/Input.tsx
@@ -1,33 +1,59 @@
-import { HTMLInputTypeAttribute } from "react"
+import {
+    HTMLInputTypeAttribute,
+    FocusEvent,
+    forwardRef,
+    ForwardedRef,
+    ChangeEvent
+} from "react"
 
 interface InputProps {
-    label: string
+    label?: string
     name: string
-    checked?: boolean
     id?: string
     value?: string | number
     type: HTMLInputTypeAttribute
     autocomplete?: string
     placeholder?: string
+    error?: string | boolean
+    onBlur?: (e: FocusEvent<HTMLInputElement>) => void
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-export function Input(props: InputProps) {
-    return (
-        <div className="field">
+export const Input = forwardRef(function Input(
+    props: InputProps,
+    ref: ForwardedRef<HTMLInputElement>
+) {
+    function renderLabel() {
+        return (
             <div className="field__label-wrapper">
-                <label className="field__input-label" htmlFor={props.id}>
+                <label
+                    htmlFor={props.id}
+                    className={`field__label ${
+                        props.error ? "field__label--error" : ""
+                    }`.trim()}
+                >
                     {props.label}
                 </label>
             </div>
+        )
+    }
+
+    return (
+        <div className="field">
+            {props.label && renderLabel()}
             <input
-                className="field__input"
-                defaultChecked={props.checked}
+                value={props.value}
+                ref={ref}
+                onBlur={props.onBlur}
+                onChange={props.onChange}
+                className={`field__input ${props.error ? "field__input--error" : ""}`.trim()}
                 id={props.id}
                 type={props.type}
                 name={props.name}
                 placeholder={props.placeholder}
                 autoComplete={props.autocomplete}
             />
+            {props.error && <p className="field__error-msg">{props.error}</p>}
         </div>
     )
-}
+})
--- src/components/checkout/components/RadioInput.tsx
+++ src/components/checkout/components/RadioInput.tsx
@@ -1,20 +1,33 @@
 "use client"
 
+import { FocusEvent, ChangeEvent, forwardRef, ForwardedRef } from "react"
+
 interface RadioInputProps {
     label: string
     name: string
     value?: string
     id?: string
     checked?: boolean
-    onChange?: () => void
+    onChange?: (e: FocusEvent<HTMLInputElement>) => void
+    onBlur?: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-export function RadioInput(props: RadioInputProps) {
+export const RadioInput = forwardRef(function RadioInput(
+    props: RadioInputProps,
+    ref: ForwardedRef<HTMLInputElement>
+) {
     return (
         <div className="radio-field">
-            <label className="radio-field__label" htmlFor={props.id}>
+            <label
+                htmlFor={props.id}
+                className={`radio-field__label ${
+                    props.checked ? "radio-field__label--checked" : ""
+                }`.trim()}
+            >
                 <input
+                    ref={ref}
                     onChange={props.onChange}
+                    onBlur={props.onBlur}
                     className="radio-field__input"
                     value={props.value}
                     checked={props.checked}
@@ -26,4 +39,4 @@ export function RadioInput(props: RadioInputProps) {
             </label>
         </div>
     )
-}
+})
