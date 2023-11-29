5d440f5b2e0ddb507e36e9e980e6e6dc3aa3bdd9
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-11-29 17:44:29 -0300

    refactor: update zodErrorFormatter to support composed names

--- src/libs/zod/index.ts
+++ src/libs/zod/index.ts
@@ -12,12 +12,20 @@ export type ErrorMap = Record<string, ErrorInfo>
 
 export function zodErrorFormater<T extends z.ZodError<any>>(error: T) {
     const errorMap = error.errors.reduce((acc: Record<string, any>, error) => {
-        const fieldName = error.path.join("").toLowerCase()
-        const captilizedFieldName = fieldName[0].toUpperCase() + fieldName.slice(1)
+        const fieldName = error.path.join("")
+        const fieldNameWithSpace = error.path.join("").replace(/([A-Z])/g, " $1")
+        const formattedFieldName = fieldNameWithSpace.split(" ").reduce((acc, name, i) => {
+            if (i === 0) {
+                acc += `${name[0].toUpperCase()}${name.slice(1)}`
+            } else {
+                acc += ` ${name.toLowerCase()}`
+            }
+            return acc
+        }, "")
 
         if (!(fieldName in acc)) {
             acc[fieldName] = {
-                message: `${captilizedFieldName} ${error.message}`.trim(),
+                message: `${formattedFieldName} ${error.message}`.trim(),
                 type: error.code
             }
         }
