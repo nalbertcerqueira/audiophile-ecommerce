2cad9082b73a6a098d6a61a3ec992c4500514664
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-12-04 18:12:17 -0300

    feat: create a RingLoader component

---
+++ src/components/shared/loaders/RingLoader.tsx
@@ -0,0 +1,9 @@
+export function RingLoader() {
+    return (
+        <div className="ring-loader">
+            <span className="ring-loader__slice" />
+            <span className="ring-loader__slice" />
+            <span className="ring-loader__slice" />
+        </div>
+    )
+}
