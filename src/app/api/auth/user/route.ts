eb770c01887cef272c69057b36148ecc00d4fa88
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-12-05 11:52:10 -0300

    feat: create '/api/auth/user' route to fetch user's info

---
+++ src/app/api/auth/user/route.ts
@@ -0,0 +1,28 @@
+import { authorizationUseCase } from "@/@core/backend/main/factories/usecases/auth/authorizationFactory"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(req: NextRequest) {
+    const authHeader = req.headers.get("Authorization")
+    const accessToken = authHeader?.split(" ")[1]
+
+    if (accessToken) {
+        const foundUser = await authorizationUseCase.execute(accessToken)
+
+        if (foundUser) {
+            const userInfo = foundUser?.toJSON()
+
+            return NextResponse.json(
+                { data: { name: userInfo.name, email: userInfo.email } },
+                { status: 200 }
+            )
+        }
+    }
+
+    const unauthorizedHeaders = { "WWW-Authenticate": 'Bearer realm="protected resource"' }
+    const unauthorizedMsg = "Unauthorized: you need valid credentials to access this conontent"
+
+    return NextResponse.json(
+        { errors: [unauthorizedMsg] },
+        { status: 401, headers: unauthorizedHeaders }
+    )
+}
