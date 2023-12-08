c9fa393c54166e9424d97ff57c2932ee6641eff9
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-12-08 13:36:53 -0300

    refactor: add a try/catch block

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -5,24 +5,29 @@ export async function GET(req: NextRequest) {
     const authHeader = req.headers.get("Authorization")
     const accessToken = authHeader?.split(" ")[1]
 
-    if (accessToken) {
-        const foundUser = await authorizationUseCase.execute(accessToken)
+    try {
+        if (accessToken) {
+            const foundUser = await authorizationUseCase.execute(accessToken)
 
-        if (foundUser) {
-            const userInfo = foundUser?.toJSON()
+            if (foundUser) {
+                const userInfo = foundUser?.toJSON()
 
-            return NextResponse.json(
-                { data: { name: userInfo.name, email: userInfo.email } },
-                { status: 200 }
-            )
+                return NextResponse.json(
+                    { data: { name: userInfo.name, email: userInfo.email } },
+                    { status: 200 }
+                )
+            }
         }
+
+        const unauthorizedHeaders = { "WWW-Authenticate": 'Bearer realm="protected resource"' }
+        const unauthorizedMsg =
+            "Unauthorized: you need valid credentials to access this conontent"
+
+        return NextResponse.json(
+            { errors: [unauthorizedMsg] },
+            { status: 401, headers: unauthorizedHeaders }
+        )
+    } catch (error: any) {
+        return NextResponse.json({ errors: [error.message] }, { status: 500 })
     }
-
-    const unauthorizedHeaders = { "WWW-Authenticate": 'Bearer realm="protected resource"' }
-    const unauthorizedMsg = "Unauthorized: you need valid credentials to access this conontent"
-
-    return NextResponse.json(
-        { errors: [unauthorizedMsg] },
-        { status: 401, headers: unauthorizedHeaders }
-    )
 }
