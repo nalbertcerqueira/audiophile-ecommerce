ccccd12d9dc7cd6a51051e15aa55af63c1c7598c
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-12-27 11:38:27 -0300

    refactor: set mobile media queries for CategoryPage component

--- src/components/category/components/TitleSection.tsx
+++ src/components/category/components/TitleSection.tsx
@@ -2,6 +2,7 @@ export function TitleSection({ title }: { title: string }) {
     return (
         <section className="title-section">
             <div className="title-section__inner-container">
+                <span className="title-section__division" />
                 <h1 className="title-section__title">{title}</h1>
             </div>
         </section>
