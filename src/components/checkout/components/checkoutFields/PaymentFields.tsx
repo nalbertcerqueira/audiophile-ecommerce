a79ee3b92828d8e37c948f32383573a87488e46e
nalbert-cerqueira <nalbertc.p@gmail.com>
2023-12-27 13:35:46 -0300

    refactor: set mobile media queries for CheckoutPage component

--- src/components/checkout/components/checkoutFields/PaymentFields.tsx
+++ src/components/checkout/components/checkoutFields/PaymentFields.tsx
@@ -13,20 +13,22 @@ export function PaymentFields(props: PaymentDetailProps) {
     return (
         <fieldset className="checkout__payment-details ">
             <legend className="checkout__group-name">{fieldsetTitle}</legend>
-            <RadioInput
-                {...register("paymentMethod")}
-                checked={currentPaymentMethod === "creditCard"}
-                value="creditCard"
-                label="Credit Card"
-                id="credit-card"
-            />
-            <RadioInput
-                {...register("paymentMethod")}
-                checked={currentPaymentMethod === "cash"}
-                value="cash"
-                label="Cash on Delivery"
-                id="cash"
-            />
+            <div className="checkout__payment-methods">
+                <RadioInput
+                    {...register("paymentMethod")}
+                    checked={currentPaymentMethod === "creditCard"}
+                    value="creditCard"
+                    label="Credit Card"
+                    id="credit-card"
+                />
+                <RadioInput
+                    {...register("paymentMethod")}
+                    checked={currentPaymentMethod === "cash"}
+                    value="cash"
+                    label="Cash on Delivery"
+                    id="cash"
+                />
+            </div>
             {currentPaymentMethod === "creditCard" && (
                 <CreditCardFields control={control} formErrors={formErrors} />
             )}
