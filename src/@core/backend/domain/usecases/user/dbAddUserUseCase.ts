dc49d689388ca6868f9ca87e9dce95f04be42a8d
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-01-03 18:00:36 -0300

    refactor: make DbAddUserUseCase also search for users in ExternalUsers repository

--- src/@core/backend/domain/usecases/user/dbAddUserUseCase.ts
+++ src/@core/backend/domain/usecases/user/dbAddUserUseCase.ts
@@ -2,20 +2,26 @@ import { User, UserProps } from "@/@core/shared/entities/user/user"
 import { AddUserRepository } from "../../repositories/user/addUserRepository"
 import { FindUserByEmailRepository } from "../../repositories/user/findUserByEmailRepository"
 import { HashService } from "../../services/crypto/hashService"
+import { FindExternalUserByEmailRepository } from "../../repositories/externalUser/findExternalUserByEmailRepository"
 
 type UserToAdd = Pick<UserProps, "email" | "name" | "password">
 
 export class DbAddUserUseCase {
     constructor(
+        private readonly findExternalUserByEmailRepository: FindExternalUserByEmailRepository,
         private readonly findUserByEmailRepository: FindUserByEmailRepository,
         private readonly addUserRepository: AddUserRepository,
         private readonly hashService: HashService
     ) {}
     public async execute(user: UserToAdd): Promise<boolean> {
         const { email, name, password } = user
-        const foundUser = await this.findUserByEmailRepository.findByEmail(email)
 
-        if (!foundUser) {
+        const [foundUser, foundExternalUser] = await Promise.all([
+            this.findUserByEmailRepository.findByEmail(email),
+            this.findExternalUserByEmailRepository.findByEmail(email)
+        ])
+
+        if (!foundUser && !foundExternalUser) {
             const hashedPassword = await this.hashService.hash(password)
             const newUser = new User({
                 name,
