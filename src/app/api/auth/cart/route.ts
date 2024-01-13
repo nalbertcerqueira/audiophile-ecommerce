3504aec96917161d5bb0f8ec686d43e65bebd1ed
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-01-13 15:16:17 -0300

    refactor: use GetCartController in /api/auth/cart (GET) route

--- src/app/api/auth/cart/route.ts
+++ src/app/api/auth/cart/route.ts
@@ -1,6 +1,6 @@
+import { getCartController } from "@/@core/backend/main/factories/controllers/cart/getCartControllerFactory"
 import { authorizationMiddleware } from "@/@core/backend/main/factories/middlewares/authorizationMiddlewareFactory"
 import { dbClearCartUseCase } from "@/@core/backend/main/factories/usecases/cart/dbClearCartFactory"
-import { dbGetCartUseCase } from "@/@core/backend/main/factories/usecases/cart/dbGetCartFactory"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
@@ -12,8 +12,14 @@ export async function GET(req: NextRequest) {
         return NextResponse.json(responseRest, { status: statusCode, headers })
     }
 
-    const cart = await dbGetCartUseCase.execute(authResponse.data.id, authResponse.data.type)
-    return NextResponse.json({ data: cart.toJSON() }, { status: 200 })
+    const { statusCode, headers, ...responseRest } = await getCartController.handle({
+        user: {
+            id: authResponse.data.id,
+            type: authResponse.data.type
+        }
+    })
+
+    return NextResponse.json(responseRest, { status: statusCode, headers })
 }
 
 export async function DELETE(req: NextRequest) {
