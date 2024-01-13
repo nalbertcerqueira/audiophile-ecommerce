79be4d25b70b647a8800fba4161bb57aac1db456
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-01-13 15:37:02 -0300

    refactor: use ClearCartController in /api/auth/cart (DELETE) route

--- src/app/api/auth/cart/route.ts
+++ src/app/api/auth/cart/route.ts
@@ -1,6 +1,6 @@
-import { getCartController } from "@/@core/backend/main/factories/controllers/cart/getCartControllerFactory"
 import { authorizationMiddleware } from "@/@core/backend/main/factories/middlewares/authorizationMiddlewareFactory"
-import { dbClearCartUseCase } from "@/@core/backend/main/factories/usecases/cart/dbClearCartFactory"
+import { clearCartController } from "@/@core/backend/main/factories/controllers/cart/clearCartControllerFactory"
+import { getCartController } from "@/@core/backend/main/factories/controllers/cart/getCartControllerFactory"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
@@ -33,6 +33,12 @@ export async function DELETE(req: NextRequest) {
         return NextResponse.json(responseRest, { status: statusCode, headers })
     }
 
-    const cart = await dbClearCartUseCase.execute(authResponse.data.id, authResponse.data.type)
-    return NextResponse.json({ data: cart.toJSON() }, { status: 200 })
+    const { statusCode, headers, ...responseRest } = await clearCartController.handle({
+        user: {
+            id: authResponse.data.id,
+            type: authResponse.data.type
+        }
+    })
+
+    return NextResponse.json(responseRest, { status: statusCode, headers })
 }
