fa5ac75ef06288e193ba9a0a10a14cc2fcb04f54
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-01-13 17:06:56 -0300

    refactor: remove unncessary files

--- src/@core/backend/main/factories/usecases/auth/authenticatedUser/dbAuthorizationFactory.ts
+++
@@ -1,9 +0,0 @@
-import { authenticatedJwtTokenService } from "../../../services/tokenServiceFactory"
-import { DbAuthorizationUseCase } from "@/@core/backend/domain/usecases/auth/authenticatedUser/dbAuthorizationUseCase"
-import { mongoUserRepository } from "../../../repositories/userRepositoryFactory"
-
-function createDbAuthorizationUseCase(): DbAuthorizationUseCase {
-    return new DbAuthorizationUseCase(authenticatedJwtTokenService, mongoUserRepository)
-}
-
-export const dbAuthorizationUseCase = createDbAuthorizationUseCase()
--- src/@core/backend/main/factories/usecases/auth/guestUser/dbGuestSessionUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { DbGuestSessionUseCase } from "@/@core/backend/domain/usecases/auth/guestUser/dbGuestSessionUseCase"
-import { guestJwtTokenService } from "../../../services/tokenServiceFactory"
-import { uuidGeneratorService } from "../../../services/idGeneratorServiceFactory"
-
-function createDbGuestSessionUseCase(): DbGuestSessionUseCase {
-    return new DbGuestSessionUseCase(uuidGeneratorService, guestJwtTokenService)
-}
-
-export const dbGuestSessionUseCase = createDbGuestSessionUseCase()
