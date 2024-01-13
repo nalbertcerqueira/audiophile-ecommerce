aca7899b377983aa1f979c19609f0fdb0b83390b
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-01-12 23:52:13 -0300

    feat: create ZodSigninValidator implementation

---
+++ src/@core/backend/infra/services/validators/signin/zpdSigninValidator.ts
@@ -0,0 +1,34 @@
+import {
+    SchemaValidatorService,
+    ValidationResult
+} from "@/@core/backend/domain/services/schemaValidator"
+import { generateCustomZodErrors } from "@/@core/shared/entities/helpers"
+import { User } from "@/@core/shared/entities/user/user"
+import { UserProps } from "@/@core/shared/entities/user/user"
+import { ZodSchema } from "zod"
+
+type SigninSchema = Pick<UserProps, "email" | "password">
+
+export class ZodSigninValidator implements SchemaValidatorService {
+    private readonly signinSchema: ZodSchema<SigninSchema>
+
+    constructor() {
+        this.signinSchema = User.userSchema.pick({ email: true, password: true })
+    }
+
+    public async validate(data: any): Promise<ValidationResult> {
+        const validationResult = this.signinSchema.safeParse(data)
+
+        if (!validationResult.success) {
+            return {
+                isValid: false,
+                errors: generateCustomZodErrors(validationResult.error, 1)
+            }
+        }
+
+        return {
+            isValid: true,
+            data: validationResult.data
+        }
+    }
+}
