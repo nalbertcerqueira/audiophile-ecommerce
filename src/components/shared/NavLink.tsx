a5c281b2a54529ce0913d7847d946da12fa1a9b1
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-01-15 16:49:20 -0300

    refactor: make the NavLink marker also mark the session in ProductPage

--- src/components/shared/NavLink.tsx
+++ src/components/shared/NavLink.tsx
@@ -13,10 +13,14 @@ export function NavLink({ path, children }: NavLinkProps) {
     const currentPathname = usePathname()
 
     function shouldRenderMarker(path: string): boolean {
-        if (currentPathname === "/" && path === "/home") {
-            return true
-        }
-        return currentPathname === path
+        if (currentPathname === "/" && path === "/") return true
+        if (!currentPathname) return false
+
+        const isPathIncluded = currentPathname?.split("/").some((nestedPath) => {
+            return nestedPath.length && nestedPath === path.slice(1)
+        })
+
+        return isPathIncluded
     }
 
     return (
