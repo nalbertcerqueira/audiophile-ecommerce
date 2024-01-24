fda6d00d98fc778b9866ec2569060f67106d0a8a
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-01-23 23:58:10 -0300

    refactor: enable the hide and show feature for the products list inside confirmation modal

--- src/components/checkout/components/ConfirmationModal.tsx
+++ src/components/checkout/components/ConfirmationModal.tsx
@@ -4,10 +4,51 @@ import { ConfirmationIcon } from "@/components/shared/icons/ConfirmationIcon"
 import { CheckoutContext } from "@/contexts/CheckoutContext"
 import { formatCurrency } from "@/utils/helpers"
 import { CartItem } from "@/components/shared/cart/CartItem"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 
 export function ConfirmationModal() {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const transitionTime = 500
+    const gap = 16
+
     const { order } = useContext(CheckoutContext)
+
+    useEffect(() => {
+        if (order?.cartItems) {
+            const { item, itemsWrapper } = getElements()
+
+            if (isOpen) {
+                const gapsHeight = gap * order.cartItems.length - 1
+                const itemsHeight = item.clientHeight * order.cartItems.length
+                itemsWrapper.style.maxHeight = `${itemsHeight + gapsHeight}px`
+            } else {
+                itemsWrapper.style.maxHeight = `${item.clientHeight}px`
+            }
+        }
+    }, [order?.cartItems, isOpen])
+
+    function getElements() {
+        const itemSelector = ".order-confirmation__items .cart-item"
+        const wrapperSelector = ".order-confirmation__items"
+        const itemsWrapper = document.querySelector(wrapperSelector) as HTMLElement
+        const item = document.querySelector(itemSelector) as HTMLElement
+
+        return { item, itemsWrapper }
+    }
+
+    function renderToggleButton() {
+        if (order && order.cartItems.length > 1) {
+            return (
+                <button
+                    onClick={() => setIsOpen((prevState) => !prevState)}
+                    className="order-confirmation__toggle-btn"
+                >
+                    {isOpen ? `View less` : `and ${order.cartItems.length - 1} other item(s)`}
+                </button>
+            )
+        }
+        return null
+    }
 
     if (!order) {
         return null
@@ -28,8 +69,11 @@ export function ConfirmationModal() {
                     </p>
                     <div className="order-confirmation__details">
                         <div className="order-confirmation__wrapper">
-                            <div className="order-confirmation__items">
-                                {order?.cartItems.map((item) => (
+                            <div
+                                style={{ gap: `${gap}px`, transition: `${transitionTime}ms` }}
+                                className="order-confirmation__items"
+                            >
+                                {order.cartItems.map((item) => (
                                     <CartItem
                                         key={item.productId}
                                         name={item.name}
@@ -42,9 +86,7 @@ export function ConfirmationModal() {
                                 ))}
                             </div>
                             <hr />
-                            <button className="order-confirmation__toggle-btn">
-                                {"and 2 other item(s)"}
-                            </button>
+                            {renderToggleButton()}
                         </div>
                         <div className="order-confirmation__total">
                             <p className="order-confirmation__total-label">GRAND TOTAL</p>
