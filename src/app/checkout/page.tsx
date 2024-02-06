22226d23ac5de70b1dd449533366760061ff42cb
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-02-06 15:10:05 -0300

    feat: display a loder to the user while fetching the Checkout page data

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,20 +1,19 @@
 import { CheckoutPageComponent } from "@/components/checkout/CheckoutPage"
 import { AuthRedirectionRoute } from "@/components/shared/AuthRedirectionRoute"
-import { PageLoaderContainer } from "@/components/shared/loaders/PageLoaderContainer"
-import { BarLoader } from "@/components/shared/loaders/BarLoader"
+
+import { Suspense } from "react"
+import Loading from "./loading"
 
 export default function CheckoutPage() {
     return (
-        <AuthRedirectionRoute
-            routeType="protected"
-            routeToRedirect="/signin"
-            fallback={
-                <PageLoaderContainer>
-                    <BarLoader />
-                </PageLoaderContainer>
-            }
-        >
-            <CheckoutPageComponent />
-        </AuthRedirectionRoute>
+        <Suspense fallback={<Loading />}>
+            <AuthRedirectionRoute
+                routeType="protected"
+                routeToRedirect="/signin"
+                fallback={<Loading />}
+            >
+                <CheckoutPageComponent />
+            </AuthRedirectionRoute>
+        </Suspense>
     )
 }
