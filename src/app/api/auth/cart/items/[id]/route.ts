c795759076178e7efcdf969552ee2467ec5cd05b
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-08-05 18:18:24 -0300

    feat: create /api/auth/cart/items/[id] (PATCH) api endpoint

--- src/app/api/auth/cart/items/[id]/route.ts
+++ src/app/api/auth/cart/items/[id]/route.ts
@@ -1,4 +1,5 @@
 import { removeCartItemController } from "@/@core/backend/main/factories/controllers/cart/removeCartItemControllerFactory"
+import { updateCartItemController } from "@/@core/backend/main/factories/controllers/cart/updateCartItemControllerFactory"
 import { authorizationMiddleware } from "@/@core/backend/main/factories/middlewares/authorizationMiddlewareFactory"
 import { NextRequest, NextResponse } from "next/server"
 
@@ -16,6 +17,28 @@ export async function DELETE(req: NextRequest, { params }: { params: { id: strin
     }
 
     const { statusCode, headers, ...responseRest } = await removeCartItemController.handle({
+        body,
+        params,
+        user: { id: authResponse.data.id, type: authResponse.data.type }
+    })
+
+    return NextResponse.json(responseRest, { status: statusCode, headers })
+}
+
+export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
+    const authorization = req.headers.get("authorization") as string
+    const body = await req.json()
+
+    const authResponse = await authorizationMiddleware.handle({
+        headers: { authorization }
+    })
+
+    if (authResponse.statusCode !== 200) {
+        const { statusCode, headers, ...responseRest } = authResponse
+        return NextResponse.json(responseRest, { status: statusCode, headers })
+    }
+
+    const { statusCode, headers, ...responseRest } = await updateCartItemController.handle({
         body,
         params,
         user: { id: authResponse.data.id, type: authResponse.data.type }
