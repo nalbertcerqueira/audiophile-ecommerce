4940a7cc21fecde3cbb6bb57b0672b61a495cb1e
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-08-31 16:30:22 -0300

    feat: create DbUpdateUserUseCase implementation

--- src/@core/backend/domain/usecases/user/userDTOs.ts
+++ src/@core/backend/domain/usecases/user/userDTOs.ts
@@ -1,3 +1,15 @@
-import { UserProps } from "@/@core/shared/entities/user/user"
+import { UserProps, UserType } from "@/@core/shared/entities/user/user"
 
 export type AddUserInputDTO = Omit<UserProps, "profileImg" | "phone">
+
+export type UpdateUserOutputDTO =
+    | (Omit<UserProps, "password"> & { id: string; type: UserType })
+    | null
+
+export interface UpdateUserInputDTO
+    extends Partial<Omit<UserProps, "email" | "profileImg" | "phone">> {
+    id: string
+    type: UserType
+    phone?: NonNullable<UserProps["phone"]>
+    profileImg?: File
+}
