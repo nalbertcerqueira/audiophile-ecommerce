0f2c81df0fa99612c5d8ad4be38b4518555962b8
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-08-31 16:36:20 -0300

    feat: create /api/auth/user/profile (PUT) api endpoint

--- src/app/api/auth/user/profile/route.ts
+++ src/app/api/auth/user/profile/route.ts
@@ -1,6 +1,7 @@
 import { guestSessionController } from "@/@core/backend/main/factories/controllers/session/guestSessionControllerFactory"
+import { updateUserProfileController } from "@/@core/backend/main/factories/controllers/user/profile/updateUserProfileControllerFactory"
 import { authorizationMiddleware } from "@/@core/backend/main/factories/middlewares/authorizationMiddlewareFactory"
-
+import { authenticatedAuthMiddleware } from "@/@core/backend/main/factories/middlewares/authenticatedAuthMiddlewareFactory"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
@@ -15,4 +16,26 @@ export async function GET(req: NextRequest) {
     }
 
     return NextResponse.json({ data: authResponse.data }, { status: 200 })
+}
+
+export async function PUT(req: NextRequest) {
+    const formData = await req.formData()
+    const body = Object.fromEntries([...formData.entries()])
+
+    const authorization = req.headers.get("authorization") as string
+    const authResponse = await authenticatedAuthMiddleware.handle({
+        headers: { authorization }
+    })
+
+    if (authResponse.statusCode !== 200) {
+        const { statusCode, headers, ...responseRest } = authResponse
+        return NextResponse.json(responseRest, { status: statusCode, headers })
+    }
+
+    const { statusCode, headers, ...responseRest } = await updateUserProfileController.handle({
+        body,
+        user: { id: authResponse.data.id, type: authResponse.data.type }
+    })
+
+    return NextResponse.json(responseRest, { status: statusCode, headers })
 }
